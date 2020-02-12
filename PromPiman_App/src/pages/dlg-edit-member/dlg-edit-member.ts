054fced91776a11c072e80be54f1dbb2f5818f31
Kritsana <tragoolphrom@gmail.com>
2020-02-12 15:41:23 +0700

    add picture

--- PromPiman_App/src/pages/dlg-edit-member/dlg-edit-member.ts
+++ PromPiman_App/src/pages/dlg-edit-member/dlg-edit-member.ts
@@ -22,7 +22,7 @@ export class DlgEditMemberPage {
       'address': [null],
       'dateOfIssue': [null],
       'dateOfExpiry': [null],
-      'picture': [null],
+      'picture': ['../../assets/imgs/man.png'],
       'phoneNumber': [null, [Validators.compose([Validators.pattern('[0-9]*')]), Validators.minLength(10), Validators.maxLength(10), Validators.required]],
       'signature': [null],
       'creationDateTime': [null],
